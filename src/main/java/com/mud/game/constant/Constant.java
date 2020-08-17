69e94c8266201c8bd878425f8c251fbbeb1605dc
mazingan <99Delove>
2020-08-17 15:19:10 +0800

    代码提交

---
+++ src/main/java/com/mud/game/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.mud.game.constant;
+
+public class Constant {
+    //强化属性增加系数
+    public static final Double STRENGTHEN_COEFFICIENT = 1.2;
+    //强化等级上限
+    public static final int MAX_LEVEL = 10;
+    //进阶属性增加系数
+    public static final Double QUALITY_COEFFICIENT = 1.5;
+}
